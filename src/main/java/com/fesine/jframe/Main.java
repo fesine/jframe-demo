bd755ef6a4f24afab56a75d7aff97dcc258cfb4c
Fesine <240944773@qq.com>
2018-02-10 13:21:45 +0800

    Initial commit

---
+++ src/main/java/com/fesine/jframe/Main.java
@@ -0,0 +1,24 @@
+package com.fesine.jframe;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @description: 类描述
+ * @author: Fesine
+ * @createTime:2018/2/10
+ * @update:修改内容
+ * @author: Fesine
+ * @updateTime:2018/2/10
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()->{
+            JFrame frame = new JFrame("welcome");
+            frame.setSize(500,500);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
